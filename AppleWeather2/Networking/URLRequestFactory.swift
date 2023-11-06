27f808f83b55dd7c482fc98775296ec10ce5c594
capta1992 <47236560+capta1992@users.noreply.github.com>
2023-11-05 22:24:25 -0800

    Implement WeatherURLRequestFactory utilizing Builder pattern- This commit introduces the WeatherurlRequestFactory structure, which depends on URLComponentsBuilder to construct URLRequest objects for various endpoints. This factory pattern centralizes request creation and leverages the builder design pattern for URL construction, enhaning code modularity and readability

--- AppleWeather2/Networking/URLRequestFactory.swift
+++ AppleWeather2/Networking/URLRequestFactory.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+protocol URLRequestFactory {
+    func makeRequest(for endpoint: EndPoint) -> URLRequest?
+}
+
+struct WeatherURLRequestFactory: URLRequestFactory {
+    private let builder: URLComponentsBuilder
+    
+    init(builder: URLComponentsBuilder) {
+        self.builder = builder
+    }
+    
+    func makeRequest(for endpoint: EndPoint) -> URLRequest? {
+        do {
+            let url: URL = try builder.build(for: endpoint)
+            return URLRequest(url: url)
+        } catch let error as NetworkingError {
+            print("DEBUG: \(error.debugMessage)")
+        } catch {
+            print(error.localizedDescription)
+        }
+        return nil
+    }
+}
