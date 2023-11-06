577e6d61f673b456d9e436e9493156d9512f5df6
capta1992 <47236560+capta1992@users.noreply.github.com>
2023-11-05 21:35:59 -0800

    Implementation Of Networking Error Object

--- AppleWeather2/Networking/NetworkingError.swift
+++ AppleWeather2/Networking/NetworkingError.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+enum NetworkingError: Error, Equatable {
+    case couldNotmakeURL
+    case noData
+    case noHttpResponse
+    case networkingFailure(Error)
+    case serialize(Error)
+    case mappingModels(Error)
+    
+    var debugMessage: String {
+        switch self {
+        case .couldNotmakeURL:
+            return "DEBUG: Could not make the url provided"
+        case .noData:
+            return "DEBUG: could not find date from API reqquest"
+        case .noHttpResponse:
+            return "DEBUG: No Http response from API request"
+        case .networkingFailure(let error):
+            return "DEBUG: Networking Failure \(error.localizedDescription)"
+        case .serialize(let error):
+            return "DEBUG: Serialization Error \(error.localizedDescription)"
+        case .mappingModels(let error):
+            return "DEBUG: Could not map wire model to data model \(error.localizedDescription)"
+        }
+    }
+    static func == (lhs: NetworkingError, rhs: NetworkingError) -> Bool {
+        lhs.debugMessage == rhs.debugMessage
+    }
+}
