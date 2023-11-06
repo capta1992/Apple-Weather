c4e914cc3e74cfc4baa26edbeef95c270262187d
capta1992 <47236560+capta1992@users.noreply.github.com>
2023-11-05 20:53:32 -0800

    Initial Commit

---
+++ AppleWeather2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleWeather2
+//
+//  Created by Lawson Falomo on 11/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
